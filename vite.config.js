9031cc0fb55410986438bb7da45041d6ac5d601d
xRudeSkinx <justrude10@gmail.com>
2025-08-09 16:21:18 -0700

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,9 @@
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// HashRouter in the app means no special rewrites needed on Vercel
+export default defineConfig({
+  base: '/',
+  plugins: [react()]
+})
