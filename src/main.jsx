819b8a8e05ccc430576ac9b3453b2b6dc5565759
xRudeSkinx <justrude10@gmail.com>
2025-08-09 16:22:48 -0700

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,17 @@
 
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import App from './pages/App.jsx'
+import Gencore from './pages/Gencore.jsx'
+import './styles.css'
+
+// HashRouter ensures /#/gencore works on any host
+createRoot(document.getElementById('root')).render(
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/gencore" element={<Gencore />} />
+    </Routes>
+  </HashRouter>
+)
